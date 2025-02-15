a5c9c629185ec544cf15ea50fe87b9c1d268acb3
Abubokkor98 <mail.abubokkor@gmail.com>
2025-02-15 12:22:59 +0600

    Install Nextjs and Setup clerk auth

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between">
+        <Link href="/" className="text-xl font-bold">
+          Meeting Room Booking
+        </Link>
+        <div className="space-x-4">
+          <Link href="/dashboard">Dashboard</Link>
+        </div>
+        <div className="space-x-4">
+        <SignedOut>
+            <SignInButton mode="modal"/>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </nav>
+  );
+}
