e6011b3f21c47382b67ccf8cbe0a0a202473bf46
Abubokkor98 <mail.abubokkor@gmail.com>
2025-02-18 11:52:09 +0600

    mpodified room delete functionality

--- src/app/components/AdminRoomCard/AdminRoomCard.jsx
+++ src/app/components/AdminRoomCard/AdminRoomCard.jsx
@@ -3,32 +3,60 @@
 import React, { useState } from "react";
 import { deleteRoom } from "@/app/lib/api";
 import UpdateRoomModal from "../UpdateRoomModal/UpdateRoomModal";
+import toast from "react-hot-toast";
 
 export default function AdminRoomCard({ room }) {
   const [showModal, setShowModal] = useState(false);
+  const { name, photo, pricePerHour, _id } = room;
 
-  const handleDelete = async () => {
-    const confirmed = window.confirm("Are you sure you want to delete this room?");
-    if (confirmed) {
-      try {
-        await deleteRoom(room._id);
-        alert("Room deleted successfully!");
-      } catch (error) {
-        alert("Failed to delete the room.");
-        console.error(error);
-      }
+  const handleDelete = async (id) => {
+    try {
+      const res = await deleteRoom(id);
+      if (res.deletedCount > 0) toast.success("Room deleted successfully!");
+    } catch (error) {
+      toast.error("Failed to delete the room.");
+      console.error(error);
     }
+  };
+
+  const modernDelete = (id) => {
+    toast((t) => (
+      <div className="flex gap-3 items-center">
+        <div>
+          <p>
+            Are you sure you want to <b>delete?</b>
+          </p>
+        </div>
+        <div className="gap-2 flex">
+          <button
+            className="bg-red-600 text-white px-3 py-1 rounded-md hover:bg-red-700"
+            onClick={() => {
+              toast.dismiss(t.id);
+              handleDelete(id);
+            }}
+          >
+            Yes
+          </button>
+          <button
+            className="bg-teal-600 text-white px-3 py-1 rounded-md hover:bg-teal-700"
+            onClick={() => toast.dismiss(t.id)}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    ));
   };
 
   return (
     <div className="bg-white shadow-lg rounded-2xl p-4 w-full sm:w-80">
       <img
-        src={room.photo}
-        alt={room.name}
+        src={photo}
+        alt={name}
         className="rounded-xl w-full h-40 object-cover mb-4"
       />
-      <h2 className="text-xl font-semibold text-teal-600">{room.name}</h2>
-      <p className="text-gray-800">Price: ৳{room.pricePerHour} per hour</p>
+      <h2 className="text-xl font-semibold text-teal-600">{name}</h2>
+      <p className="text-gray-800">Price: ৳{pricePerHour} per hour</p>
 
       <div className="mt-4 flex justify-between">
         <button
@@ -38,7 +66,7 @@ export default function AdminRoomCard({ room }) {
           Update
         </button>
         <button
-          onClick={handleDelete}
+          onClick={() => modernDelete(_id)}
           className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700"
         >
           Delete
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "./provider/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
+        <Toaster position="top-center" />
           <main>{children}</main>
         </AuthProvider>
       </body>
